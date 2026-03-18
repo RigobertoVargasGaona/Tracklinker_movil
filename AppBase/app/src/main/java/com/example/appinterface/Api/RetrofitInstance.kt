d13fbd2ae750c565bff3eb6e900ca1a8c7b10b3b
DevJuan001 <juanesyt7@gmail.com>
2026-03-17 21:50:20 -0500

    feat: enhance RetrofitInstance to include UsersApiService and update base URL constants

--- AppBase/app/src/main/java/com/example/appinterface/Api/RetrofitInstance.kt
+++ AppBase/app/src/main/java/com/example/appinterface/Api/RetrofitInstance.kt
@@ -1,5 +1,6 @@
 package com.example.appinterface.Api
 
+import com.example.appinterface.Api.Services.UsersApiService
 import com.example.appinterface.Api.Services.WarrantiesApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -8,14 +9,23 @@ import kotlin.jvm.java
 
 object RetrofitInstance {
 
-    private const val BASE_URL_APIKOTLIN = "http://10.0.2.2:8080"
+    private const val BASE_URL_KOTLIN_API = "http://10.0.2.2:8080"
+    private const val BASE_URL_PYTHON_API = "http://10.0.2.2:8000"
 
     val api2kotlin: WarrantiesApiService by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL_APIKOTLIN)
+            .baseUrl(BASE_URL_KOTLIN_API)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(WarrantiesApiService::class.java)
+    }
+
+    val usersApi: UsersApiService by lazy {
+        Retrofit.Builder()
+        .baseUrl(BASE_URL_KOTLIN_API)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(UsersApiService::class.java)
     }
 
 }
