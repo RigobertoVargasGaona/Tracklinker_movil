3708edb6ff323f8504734a5672c4e737dc84f985
EdwinQuenguan <edwinquenguan14@gmail.com>
2026-03-20 21:44:20 -0500

    fix: update RetrofitInstance import to use the correct class reference

--- AppBase/app/src/main/java/com/example/appinterface/features/warranties/WarrantiesActivity.kt
+++ AppBase/app/src/main/java/com/example/appinterface/features/warranties/WarrantiesActivity.kt
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appinterface.Adapter.adapterWarranties.WarrantyAdapter
 import com.example.appinterface.Adapter.adapterWarranties.OnWarrantyClickListener
-import com.example.appinterface.Api.RetrofitInstance
+import com.example.appinterface.Api.RetrofitInstance.RetrofitInstance
 import com.example.appinterface.Api.Models.DataResponseWarranty
 import com.example.appinterface.MainActivity
 import com.example.appinterface.R
