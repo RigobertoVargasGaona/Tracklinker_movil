0cf22585072fa6e93eaee428986d5c110cc84954
DevJuan001 <juanesyt7@gmail.com>
2026-03-21 21:34:28 -0500

    refactor: update DataResponseCategory model to include success and message fields, removing category_id and category_name

--- AppBase/app/src/main/java/com/example/appinterface/Api/Models/DataResponseCategory.kt
+++ AppBase/app/src/main/java/com/example/appinterface/Api/Models/DataResponseCategory.kt
@@ -1,7 +1,6 @@
 package com.example.appinterface.Api.Models
 
 data class DataResponseCategory(
-    val category_id: Int = 0,
-    val category_name: String,
-    val category_description: String = ""
+    val success: Boolean,
+    val message: String,
 )
