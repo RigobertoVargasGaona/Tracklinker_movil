0944045c60719241c71aa8f865d70fde3335aecf
DevJuan001 <juanesyt7@gmail.com>
2026-03-21 21:26:26 -0500

    feat: enhance Output Orders module with detailed output order management, including CRUD operations and UI interactions

--- AppBase/app/src/main/java/com/example/appinterface/Adapter/outputOrders/OutputOrdersListener.kt
+++ AppBase/app/src/main/java/com/example/appinterface/Adapter/outputOrders/OutputOrdersListener.kt
@@ -1,4 +1,9 @@
 package com.example.appinterface.Adapter.outputOrders
 
+import com.example.appinterface.Api.Models.OutputOrder
+
 interface OutputOrdersListener {
+    fun onShowInfo(outputOrder: OutputOrder)
+    fun onEditOutputOrder(outputOrder: OutputOrder)
+    fun onDelete(outputOrder: OutputOrder)
 }
--- AppBase/app/src/main/java/com/example/appinterface/Api/Models/OutputOrder.kt
+++ AppBase/app/src/main/java/com/example/appinterface/Api/Models/OutputOrder.kt
@@ -1,4 +1,22 @@
 package com.example.appinterface.Api.Models
 
-class OutputOrder {
-}
+data class OutputOrder(
+    val out_order_id: Int,
+    val out_order_date: String,
+    val output_details_id: Int,
+    val product_serial: String,
+    val out_product_garanty: String,
+    val product_transformation: String,
+    val product_detail_description: String,
+    val product_detail_model: String,
+    val product_brand_name: String,
+)
+
+data class CreateOutputOrder(
+    val serial: String,
+    val brand: String,
+    val transfomation: String,
+    val product_garanty: String,
+    val product_details_id: Int,
+    val amount: Int
+)
