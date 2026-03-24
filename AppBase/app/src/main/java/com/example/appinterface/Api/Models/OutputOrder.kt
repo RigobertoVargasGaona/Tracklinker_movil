fa2043bbf4127a946f8cd5e13fb3947862eaae1d
DevJuan001 <juanesyt7@gmail.com>
2026-03-24 17:40:57 -0500

    feat: add out_order_status field to OutputOrder and create UpdateOutputOrder data class

--- AppBase/app/src/main/java/com/example/appinterface/Api/Models/OutputOrder.kt
+++ AppBase/app/src/main/java/com/example/appinterface/Api/Models/OutputOrder.kt
@@ -10,13 +10,19 @@ data class OutputOrder(
     val product_detail_description: String,
     val product_detail_model: String,
     val product_brand_name: String,
+    val out_order_status: Int
 )
 
 data class CreateOutputOrder(
-    val serial: String,
-    val brand: String,
-    val transfomation: String,
-    val product_garanty: String,
-    val product_details_id: Int,
-    val amount: Int
+    val product_serial: String,
+    val product_transformation: String,
+    val out_product_garanty: String,
+)
+
+data class UpdateOutputOrder(
+    val out_order_id: Int,
+    val product_serial: String,
+    val product_transformation: String,
+    val out_product_garanty: String,
+    val out_order_status: Int,
 )
