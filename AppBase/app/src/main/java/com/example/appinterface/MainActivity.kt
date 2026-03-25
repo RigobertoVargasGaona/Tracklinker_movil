9a19bc60718772639a4ab4dbf6408a41b30a1be6
DevJuan001 <juanesyt7@gmail.com>
2026-03-25 14:52:05 -0500

    feat: integrate Retrofit API calls in MainActivity to fetch and display charts and KPIs dynamically

--- AppBase/app/src/main/java/com/example/appinterface/MainActivity.kt
+++ AppBase/app/src/main/java/com/example/appinterface/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.appinterface
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appinterface.Adapter.home.HomeAdapter
 import com.example.appinterface.Adapter.home.HomeItem
+import com.example.appinterface.Api.Models.DataResponseCharts
+import com.example.appinterface.Api.Models.DataResponseKpis
+import com.example.appinterface.Api.RetrofitInstance
 import com.example.appinterface.helpers.BottomNavHelper
 import com.github.mikephil.charting.data.Entry
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity: AppCompatActivity() {
 
@@ -17,38 +24,67 @@ class MainActivity: AppCompatActivity() {
         setContentView(R.layout.main_activity)
         findViewById<TextView>(R.id.top_user_gretting).setText("Hola, Agustín")
 
-        val kpis = listOf(
-            HomeItem.Kpi("Productos", "$ 900,000", "+12%"),
-            HomeItem.Kpi("Ventas", "$ 12,400", "+8%"),
-            HomeItem.Kpi("Clientes", "14", "+3%"),
-        )
-
-        val charts = listOf(
-            HomeItem.Chart(
-                title = "Ventas por semana",
-                entries = listOf(
-                    Entry(0f, 200f),
-                    Entry(1f, 450f),
-                    Entry(2f, 300f),
-                    Entry(3f, 700f),
-                    Entry(4f, 500f),
-                )
-            )
-        )
-
-        // Kpis
-        val kpisRecycler = findViewById<RecyclerView>(R.id.KpisRecycler)
-        kpisRecycler.layoutManager = LinearLayoutManager(
-            this, LinearLayoutManager.HORIZONTAL, false
-        )
-
-        kpisRecycler.adapter = HomeAdapter(kpis)
+        BottomNavHelper.setup(this, R.id.nav_home)
 
         // Charts
         val chartsRecycler = findViewById<RecyclerView>(R.id.chartRecycler)
-        chartsRecycler.layoutManager = LinearLayoutManager(this)
-        chartsRecycler.adapter = HomeAdapter(charts)
+        chartsRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
+        val chartsAdapter = HomeAdapter(mutableListOf())
+        chartsRecycler.adapter = chartsAdapter
 
-        BottomNavHelper.setup(this, R.id.nav_home)
+        RetrofitInstance.reportsApi.getChartInfo().enqueue(object : Callback<DataResponseCharts> {
+            override fun onResponse(call: Call<DataResponseCharts>, response: Response<DataResponseCharts>) {
+                if (response.isSuccessful) {
+                    val body = response.body()?.data ?: return
+
+                    val entries = body.mapIndexed { index, item ->
+                        Entry(index.toFloat(), item.products.toFloat())
+                    }
+
+                    val charts = listOf(
+                        HomeItem.Chart(
+                            title = "Productos Agregados por mes del año 2024",
+                            entries = entries
+                        )
+                    )
+
+                    runOnUiThread {
+                        chartsAdapter.updateData(charts)
+                    }
+                }
+            }
+            override fun onFailure(call: Call<DataResponseCharts>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Error al intentar cargar la informacion", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Kpis
+        val kpisRecycler = findViewById<RecyclerView>(R.id.KpisRecycler)
+        kpisRecycler.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+        val kpisAdapter = HomeAdapter(mutableListOf())
+        kpisRecycler.adapter = kpisAdapter
+
+        RetrofitInstance.reportsApi.getKpisInfo().enqueue(object : Callback<DataResponseKpis> {
+            override fun onResponse(call: Call<DataResponseKpis>, response: Response<DataResponseKpis>) {
+                if (response.isSuccessful) {
+                    val data = response.body()?.data ?: return
+
+                    val kpis = data.flatMap { item ->
+                        mutableListOf<HomeItem>(
+                            HomeItem.Kpi("Productos", item.products.toString(), "+0"),
+                            HomeItem.Kpi("Clientes", item.clients.toString(), "+0"),
+                            HomeItem.Kpi("Usuarios", item.users.toString(), "+0"),
+                        )
+                    }
+
+                    runOnUiThread {
+                        kpisAdapter.updateData(kpis)
+                    }
+                }
+            }
+            override fun onFailure(call: Call<DataResponseKpis>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Error al intentar cargar la informacion", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
